06e20b813e46ef708e7baec8e80be178e060863e
Rolake <morolaanney@gmail.com>
2025-03-16 21:19:44 +0000

    Added key vault key

--- main.tf
+++ main.tf
@@ -79,7 +79,6 @@ module "function_app" {
   tenant_id        = data.azurerm_client_config.current.tenant_id
   key_vault_id     = azurerm_key_vault.functions_kv.id
   identity         = azurerm_user_assigned_identity.identity.id
-  key_vault_key_id = azurerm_key_vault_key.key.id
 
   function_configurations = {
     "pricing" = {
