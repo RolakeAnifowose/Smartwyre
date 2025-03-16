a018465f603906496d8b2471e588f8c0a2b5d1de
Rolake <morolaanney@gmail.com>
2025-03-16 16:06:25 +0000

    Updated pipeline

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "smartwyreterraformstate"
     container_name = "tfstate"
-    resource_group_name = "CloudOps-Smartwyre-resource-group"
+    resource_group_name = "CloudOps-Smartwyre-terraform-backend-group"
     key = "terraform.tfstate"
   }
 
