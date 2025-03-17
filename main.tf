b24a937aa0ff3ba50d9d9dadb1b43576b1ff8838
Rolake <morolaanney@gmail.com>
2025-03-17 01:41:33 +0000

    Latest module version

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "azurerm_app_configuration" "functions_appcfg" {
 }
 
 module "function_app" {
-  source = "github.com/RolakeAnifowose/smartwyre-function-module?ref=v0.0.13"
+  source = "github.com/RolakeAnifowose/smartwyre-function-module?ref=v0.0.14"
 
   functions      = toset(var.function_app_names)
   resource_group = azurerm_resource_group.functions_rg
